e1de3192cea997fd9e22a0b6f25bfceb82ee41de
Nic Wortel <nic@nicwortel.nl>
2018-08-15 13:40:31 +0200

    Move the loading of configuration files to the Application class

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -3,7 +3,10 @@ declare(strict_types=1);
 
 namespace Leviy\ReleaseTool\Console;
 
+use Symfony\Component\Config\FileLocator;
 use Symfony\Component\Console\Application as SymfonyApplication;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 final class Application extends SymfonyApplication
 {
@@ -11,8 +14,18 @@ final class Application extends SymfonyApplication
 
     private const VERSION = '@package_version@';
 
-    public function __construct()
+    public function __construct(ContainerBuilder $container)
     {
         parent::__construct(self::NAME, self::VERSION);
+
+        $this->buildContainer($container);
+    }
+
+    private function buildContainer(ContainerBuilder $container): void
+    {
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../../config'));
+        $loader->load('services.yaml');
+        $loader->load('github.yaml');
+        $loader->load('commands.yaml');
     }
 }
